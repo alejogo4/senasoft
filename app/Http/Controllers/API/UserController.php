c2da0f672c54b022550bfd3714ac8b23225f3ca1
Alejandro Giraldo Duque <alejo.go4@hotmail.com>
2019-07-09 15:41:57 -0500

    Actualizar rutas

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -66,32 +66,11 @@ class UserController extends Controller
      * 
      * @return \Illuminate\Http\Response 
      */
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 401);
-        }
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('senasoft')->accessToken;
-        $success['name'] =  $user->name;
-        return response()->json(['success' => $success], $this->successStatus);
-    }
+   
     /** 
      * details api 
      * 
      * @return \Illuminate\Http\Response 
      */
-    public function details()
-    {
-        $user = Auth::user();
-        return response()->json(['success' => $user], $this->successStatus);
-    }
+  
 }
--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 |
 */
 Route::post('login', 'API\UserController@login');
-Route::post('register', 'API\UserController@register');
+
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::post('details', 'API\UserController@details');
+   
 });
