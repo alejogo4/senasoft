61fe13bad942996566235ad3e29c3c042e00adee
hmaya10 <hmaya10@hotmail.com>
2019-07-11 10:13:47 -0500

    carga

--- resources/views/web/equipo/list.blade.php
+++ resources/views/web/equipo/list.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
+<!-- Modal -->
+<div class="modal fade" id="modal_registro" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content" >
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <img src="/images/instructivo_equipos.JPG" alt="Recomendaciones Instructivo" width="100%">
+            </div>
+        </div>
+    </div>
+</div>
         <div class="row">
             <div class="col-xl-12">
                 <!-- Example 01 -->
@@ -29,7 +42,7 @@
                                 <td>{{$equipo->centro->nombre_centro}}</td>
                                 <td>{{$equipo->id}}</td>  
                                 <td><div class="col-md-3">
-                                            <button type="button" class="section-lyla btn btn-formato btn-lg" data-toggle="modal" data-target="#modal_recomen">
+                                            <button type="button" class="section-lyla btn btn-formato btn-lg" data-toggle="modal" data-target="#modal_registro">
                                                 Ver
                                             </button>
                                 </div> </td> 
