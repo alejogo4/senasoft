194eff6990a0707d740f0b5c1bffd54e66c9b8b0
Juan David <j-deiby@hotmail.com>
2019-08-05 19:03:58 -0500

    mejora modal equipo

--- resources/views/app/equipo/list.blade.php
+++ resources/views/app/equipo/list.blade.php
@@ -10,7 +10,7 @@
     <div class="modal-dialog modal-lg" style="max-width: 90%;" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                Listado de equipos
+                <h1>Listado de equipos</h1>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                         aria-hidden="true">&times;</span></button>
             </div>
