6e9c4f0ae9f8e8b48fd6d87a79e340cd031b8102
Alejandro Giraldo Duque <alejo.go4@hotmail.com>
2019-08-13 16:23:49 -0500

    Impresion Escarapelas Vista

--- resources/views/app/registro/escarapela.blade.php
+++ resources/views/app/registro/escarapela.blade.php
@@ -23,27 +23,56 @@
             height: 50vh;
             float:left;
             overflow: auto;
-            background: url('/images/escarapela.jpg')  no-repeat;
+            background: url('/images/escarapela_blank.jpg')  no-repeat;
             background-size: contain;
             background-position: center center;
             box-sizing: border-box;
+            text-align: center;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            color: white;
+            position: relative;
+        }
+
+        .escarapela .text{
+            position: absolute;
+            left: 0px;
+            right: 0px;
+            width: 80%;
+            top: 90px;
+        }
+
+        .escarapela .name{
+            margin-top: 15%;
+            font-size: 15px;
+        }
+
+        .escarapela h3{
+            font-size: 12px;
         }
     </style>
 </head>
 <body>
-
-    <div class="container">
-            <div class="escarapela"></div>
-            <div class="escarapela"></div>
-            <div class="escarapela"></div>
-            <div class="escarapela"></div>
-    </div>
-    <div class="container">
-            <div class="escarapela"></div>
-            <div class="escarapela"></div>
-            <div class="escarapela"></div>
-            <div class="escarapela"></div>
-    </div>
+    <?php $cont=0 ?>
+    @foreach($personas as $key => $value)
+        @if(($key+1)%4 == 0 || $key+1 == 1)
+            <div class="container">
+            <?php $cont=0 ?>
+        @endif
+        <?php $cont++ ?>
+        <div class="escarapela">
+            <div class="text">
+                <h2 class="name">{{$value->nombres}}</h2>
+                <h3 class="lastname">{{$value->apellidos}}</h3>
+                <p>No. Doc {{$value->documento}}</p>
+                <h3></h3>
+            </div>
+        </div>
+ 
+        @if($cont == 4)
+            </div>
+        @endif
+    @endforeach
+    
     
 </body>
 </html>
