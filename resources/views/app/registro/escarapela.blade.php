ba7bee0a06cd5342d3eb218767f320336d0c354c
Alejandro Giraldo Duque <alejo.go4@hotmail.com>
2019-08-15 16:32:41 -0500

    Organizar escarapela vista

--- resources/views/app/registro/escarapela.blade.php
+++ resources/views/app/registro/escarapela.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,23 +8,23 @@
     <title>Escarapelas</title>
 
     <style>
-        *{
+        * {
             margin: 0 auto;
-            padding:0;
+            padding: 0;
         }
 
-        .container{
+        .container {
             width: 100%;
             height: 100vh;
         }
 
-       
-        .escarapela{
+
+        .escarapela {
             width: 50%;
             height: 50vh;
-            float:left;
+            float: left;
             overflow: auto;
-            background: url('/images/escarapela_blank.jpg')  no-repeat;
+            background: url('/images/escarapela_blank.jpg') no-repeat;
             background-size: contain;
             background-position: center center;
             box-sizing: border-box;
@@ -33,7 +34,7 @@
             position: relative;
         }
 
-        .escarapela .text{
+        .escarapela .text {
             position: absolute;
             left: 0px;
             right: 0px;
@@ -41,22 +42,65 @@
             top: 90px;
         }
 
-        .escarapela .name{
+        .escarapela .name {
             margin-top: 15%;
             font-size: 15px;
         }
 
-        .escarapela h3{
+        .escarapela h3 {
             font-size: 12px;
         }
     </style>
 </head>
+
 <body>
-    <?php $cont=0 ?>
+
+
+    <style type="text/css">
+        .tftable {
+            font-size: 12px;
+            color: #333333;
+            width: 100%;
+            border-width: 1px;
+            border-color: #729ea5;
+            border-collapse: collapse;
+        }
+
+        .tftable tr {
+            
+        }
+
+        .tftable td {
+            font-size: 12px;
+            border-width: 1px;
+            border-style: solid;
+            background: url('/images/escarapela_blank.png') no-repeat;
+            background-size: 100%;
+            
+        }
+       
+    </style>
+
+    <table class="tftable" border="1">
+        <tbody>
+            <tr>
+                <td height="49%">
+                  
+                </td>
+                <td height="49%">Row:1 Cell:2</td>
+            </tr>
+            <tr>
+                <td height="49%">Row:1 Cell:1</td>
+                <td height="49%">Row:1 Cell:2</td>
+            </tr>
+        </tbody>
+
+    </table>
+    <?php $cont = 0 ?>
     @foreach($personas as $key => $value)
-        @if(($key+1)%4 == 0 || $key+1 == 1)
-            <div class="container">
-            <?php $cont=0 ?>
+    @if(($key+1)%4 == 0 || $key+1 == 1)
+    <div class="container">
+        <?php $cont = 0 ?>
         @endif
         <?php $cont++ ?>
         <div class="escarapela">
@@ -67,12 +111,13 @@
                 <h3></h3>
             </div>
         </div>
- 
+
         @if($cont == 4)
-            </div>
-        @endif
+    </div>
+    @endif
     @endforeach
-    
-    
+
+
 </body>
+
 </html>
