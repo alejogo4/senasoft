e8724ac656376282b25059e5ff1ba96d17a639f4
Alejandro Giraldo Duque <alejo.go4@hotmail.com>
2019-09-13 13:51:09 -0500

    fix Bug Controller

--- app/Http/Controllers/API/RefrigerioController.php
+++ app/Http/Controllers/API/RefrigerioController.php
@@ -6,35 +6,47 @@ use App\Models\Persona;
 use App\Models\Refrigerio;
 use App\Models\TipoRefrigerio;
 use App\Models\Usref;
+use App\Http\Controllers\API\Transformers\APITransformer;
 class RefrigerioController extends Controller
 {
     
     function comprobarRefrigerio(Request $request){
         //Obtener usuario y refrigerio
-        $user = Persona::select('tbl_usuario.*')->where('cedula', $request->cedula)->first();
+		$ok=null;
+		$mensaje = null;
+        $user = Persona::select('tbl_persona.*')->where('documento', $request->cedula)->first();
+		if($user == null){
+			$ok = false;
+            $mensaje  = 'El usuario no se ha encontrado en la base de datos';
+            return (new APITransformer)->transform(["ok"=>$ok, "datos"=>$mensaje]);
+        }
+        
         $refrigerio = Refrigerio::select('tbl_refrigerio.*')->whereRaw('? between fecha_inicial AND fecha_final', [now()])->first();
         //Comprobar si el usuario ya reclamo o no
-        $us_ref = Usref::select('tbl_us_ref.*')->where('usuarios_id', $user->id)->where('refrigerios_id', $refrigerio->id)->first();
+		if($refrigerio == null){
+			
+			$ok = false;
+            $mensaje  = 'En este horario no hay un refrigerio programado';
+            return (new APITransformer)->transform(["ok"=>$ok, "datos"=>$mensaje]);
+		}
+		
+		$us_ref = Usref::where('persona_id', $user->id)->where('refrigerios_id', $refrigerio->id)->first();
+		
         //Comprobar el tipo de refrigerio
         $tipo_refrigerio = TipoRefrigerio::select('tbl_tipo_refrigerio.*')->where('id', $refrigerio->tipo_ref_id)->first();
         //Validacion
         if(is_null($us_ref) || empty($us_ref)){
             
             $newData_us_ref = new Usref;
-            $newData_us_ref->usuarios_id = $user->id;
+            $newData_us_ref->persona_id = $user->id;
             $newData_us_ref->refrigerios_id = $refrigerio->id;
             $newData_us_ref->estado = 1;
             $newData_us_ref->save();
-            return response()->json([
-                'ok' => true,
-                'mensaje' => 'El ' . $tipo_refrigerio->tipo .' se ha registrado correctamente.'
-            ]);
+            return (new APITransformer)->transform(["ok"=>true, "datos"=>'El ' . $tipo_refrigerio->tipo .' se ha registrado correctamente.']);
+
         } else{
-            
-            return response()->json([
-                'ok' => false,
-                'Mensaje' => 'ya ha reclamado el '. $tipo_refrigerio->tipo
-            ]);
+            return (new APITransformer)->transform(["ok"=>false, "datos"=>'ya ha reclamado el '. $tipo_refrigerio->tipo]);
+        
         }
     }
 }
