d3bf36388c4e140d333c3fab4f15b92bc66962b3
Juan David <j-deiby@misena.edu.co>
2019-10-16 00:05:15 -0500

    equipos

--- app/Http/Controllers/API/ProyectoController.php
+++ app/Http/Controllers/API/ProyectoController.php
@@ -16,7 +16,7 @@ class ProyectoController extends Controller
         $topProyectos = Proyecto::select("tbl_proyecto.*","tbl_centro.nombre_centro", "tbl_regional.nombre_regional")
         ->join("tbl_centro", "tbl_centro.id", "=", "tbl_proyecto.centro_id")
         ->join("tbl_regional", "tbl_regional.id", "=", "tbl_centro.regional_id")
-        ->where('tbl_proyecto.estado',0)->orderBy('tbl_proyecto.puntaje', 'desc')->take($cantidadDeProyectos)->get();
+        ->where('tbl_proyecto.estado',1)->orderBy('tbl_proyecto.puntaje', 'desc')->take($cantidadDeProyectos)->get();
         return (new APITransformer)->transform(["ok"=>true, "datos"=>$topProyectos]);
     }
 
