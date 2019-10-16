47f6bc13caf73411a3ee3066130792a817390842
AndresFelipeGG <51917154+AndresFelipeGG@users.noreply.github.com>
2019-10-16 15:26:17 -0500

    finalistas

--- database/seeds/PorcentajeSeeder.php
+++ database/seeds/PorcentajeSeeder.php
@@ -69,6 +69,181 @@ class PorcentajeSeeder extends Seeder
             'fase_id'=>4,
             'porcentaje'=>30
         ]);
+
+        // Desarrollo web 
+        Porcentaje::create([
+            'categoria_id'=>3,
+            'fase_id'=>1,
+            'porcentaje'=>30
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>3,
+            'fase_id'=>2,
+            'porcentaje'=>30
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>3,
+            'fase_id'=>3,
+            'porcentaje'=>30
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>3,
+            'fase_id'=>4,
+            'porcentaje'=>10
+        ]);
+
+        // Desarrollo móvil 
+        Porcentaje::create([
+            'categoria_id'=>4,
+            'fase_id'=>1,
+            'porcentaje'=>25
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>4,
+            'fase_id'=>2,
+            'porcentaje'=>25
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>4,
+            'fase_id'=>3,
+            'porcentaje'=>25
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>4,
+            'fase_id'=>4,
+            'porcentaje'=>25
+        ]);
         
+        // Redes y mantenimiento
+        Porcentaje::create([
+            'categoria_id'=>5,
+            'fase_id'=>1,
+            'porcentaje'=>40
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>5,
+            'fase_id'=>2,
+            'porcentaje'=>10
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>5,
+            'fase_id'=>3,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>5,
+            'fase_id'=>4,
+            'porcentaje'=>30
+        ]);
+
+        // Producción multimedia
+        Porcentaje::create([
+            'categoria_id'=>6,
+            'fase_id'=>1,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>6,
+            'fase_id'=>2,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>6,
+            'fase_id'=>3,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>6,
+            'fase_id'=>4,
+            'porcentaje'=>40
+        ]);
+
+        // Videojuegos
+        Porcentaje::create([
+            'categoria_id'=>7,
+            'fase_id'=>1,
+            'porcentaje'=>50
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>7,
+            'fase_id'=>2,
+            'porcentaje'=>10
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>7,
+            'fase_id'=>3,
+            'porcentaje'=>15
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>7,
+            'fase_id'=>4,
+            'porcentaje'=>25
+        ]);
+
+        // Animación 3D
+        Porcentaje::create([
+            'categoria_id'=>8,
+            'fase_id'=>1,
+            'porcentaje'=>40
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>8,
+            'fase_id'=>2,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>8,
+            'fase_id'=>3,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>8,
+            'fase_id'=>4,
+            'porcentaje'=>20
+        ]);
+
+        // Producción medios audiovisuales
+        Porcentaje::create([
+            'categoria_id'=>9,
+            'fase_id'=>1,
+            'porcentaje'=>15
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>9,
+            'fase_id'=>2,
+            'porcentaje'=>15
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>9,
+            'fase_id'=>3,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>9,
+            'fase_id'=>4,
+            'porcentaje'=>30
+        ]);
+
+        // IDEATIC
+        Porcentaje::create([
+            'categoria_id'=>10,
+            'fase_id'=>1,
+            'porcentaje'=>15
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>10,
+            'fase_id'=>2,
+            'porcentaje'=>15
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>10,
+            'fase_id'=>3,
+            'porcentaje'=>20
+        ]);
+        Porcentaje::create([
+            'categoria_id'=>10,
+            'fase_id'=>4,
+            'porcentaje'=>50
+        ]);
     }
 }
